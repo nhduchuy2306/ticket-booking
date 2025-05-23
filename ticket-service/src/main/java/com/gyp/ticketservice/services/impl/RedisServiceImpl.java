dd91c3b6f82e383e70e53589156b696ac65e1cfc
Huy Nguyen <nguyenhuy1323614@gmail.com>
2025-05-23 12:26:32 +0700

    update docker

--- ticket-service/src/main/java/com/gyp/ticketservice/services/impl/RedisServiceImpl.java
+++ ticket-service/src/main/java/com/gyp/ticketservice/services/impl/RedisServiceImpl.java
@@ -2,21 +2,30 @@ package com.gyp.ticketservice.services.impl;
 
 import com.gyp.ticketservice.services.RedisService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RedisServiceImpl implements RedisService {
+	@Value("${redis.open:false}")
+	private boolean redisActionOn;
+
 	private final RedisTemplate<String, Object> redisTemplate;
 
 	@Override
 	public void saveData(String key, Object value) {
-		redisTemplate.opsForValue().set(key, value);
+		if(redisActionOn) {
+			redisTemplate.opsForValue().set(key, value);
+		}
 	}
 
 	@Override
 	public Object getData(String key) {
-		return redisTemplate.opsForValue().get(key);
+		if(redisActionOn) {
+			return redisTemplate.opsForValue().get(key);
+		}
+		return null;
 	}
 }
