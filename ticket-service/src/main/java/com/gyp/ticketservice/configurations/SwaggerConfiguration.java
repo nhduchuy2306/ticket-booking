effd7f42af055fc2adb29947e10c7e19354bc883
Huy Nguyen <nguyenhuy1323614@gmail.com>
2025-08-06 00:23:04 +0700

    Update Swagger for ticket

---
+++ ticket-service/src/main/java/com/gyp/ticketservice/configurations/SwaggerConfiguration.java
@@ -0,0 +1,24 @@
+package com.gyp.ticketservice.configurations;
+
+import java.util.List;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.servers(List.of(
+						new Server().url("http://localhost:9999/tickets").description("Ticket Service"),
+						new Server().url("http://localhost:9002").description("Ticket Service"))
+				)
+				.info(new Info().title("Ticket Service API")
+						.version("1.0")
+						.description("API Documentation"));
+	}
+}
