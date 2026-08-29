0421f31f93fc39c78f767c49b98ea0af112f8836
huynhd <nguyenhuy1323614@gmail.com>
2026-08-29 13:30:44 +0700

    Clean source code

--- commons/common-base/src/main/java/com/gyp/common/configurations/CorsConfiguration.java
+++ commons/common-base/src/main/java/com/gyp/common/configurations/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.gyp.common.configurations;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,7 +13,7 @@ public class CorsConfiguration {
 	public WebMvcConfigurer corsConfigure() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
 				registry.addMapping("/**")
 						.allowedOrigins("*")
 						.allowedMethods("*")
